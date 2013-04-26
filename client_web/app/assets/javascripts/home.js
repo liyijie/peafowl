592b0e98c715aa43a5f18bfd468199f0a0dffa55
magic <huronghai2008@gmail.com>
2013-04-26 22:22:32 +0800

    incr and desc order item

--- client_web/app/assets/javascripts/home.js
+++ client_web/app/assets/javascripts/home.js
@@ -14,66 +14,21 @@ $(function() {
     },
 
     increaseAmmount: function() {
-      this.save({"ammount": this.get("ammount") + 1, "active": true});
+      this.active();
+      this.save({"ammount": this.get("ammount") + 1});
       return this.get("ammount");
     },
 
     descreaseAmmount: function() {
-      this.save({"ammount": this.get("ammount") - 1, "active": true});
+      this.active();
+      this.save({"ammount": this.get("ammount") - 1});
       return this.get("ammount");
+    },
+
+    active: function() {
+      this.save({active: true});
     }
   });
-
-  var OrderItemList = Backbone.Collection.extend({
-    model: OrderItem,
-    //url: "/orders",
-    localStorage: new Backbone.LocalStorage("order-items"),
-
-    totalPrice: function() {
-      var totalPrice = 0;
-      this.each(function(order) {
-        totalPrice += order.totalPrice();
-      });
-      return totalPrice;
-    },
-
-    increaseOrder: function(order) {
-      var existOrder = this.findOrder(order.foot_id);
-      if (existOrder) {
-        existOrder.increaseAmmount();
-      } else {
-        this.create(order);
-      }
-    },
-
-    descreaseOrder: function(order) {
-      var existOrder = this.findOrder(order.foot_id);
-      var ammount = existOrder.descreaseAmmount();
-
-      if (ammount <= 0) {
-        existOrder.destroy();
-        this.first().set({active: true});
-      }
-    },
-
-    findOrder: function(foot_id) {
-      return this.find(function(order) {
-        return order.get("foot_id") == foot_id;
-      });
-    },
-
-    checkout: function() {
-      $.postJSON("/orders", this.toJSON(), function(response) {
-        alert(response);
-      }, 'json');
-    },
-
-    activeItem: function() {
-      return this.where({active: true}).get(0);
-    }
-  });
-
-  var orderItemList = new OrderItemList;
 
   var OrderItemView = Backbone.View.extend({
     tagName: "tr",
@@ -90,30 +45,88 @@ $(function() {
 
     render: function() {
       this.$el.html(this.template(this.model.toJSON()));
-      this.activeItem();
-      this.showTotalPrice();
-      return this;
-    },
-
-    activeItem: function() {
-      $("#order-list tbody tr").removeClass("error");
       this.$el.toggleClass("error", this.model.get("active"));
-    },
-
-    showTotalPrice: function() {
+      this.$el.attr("data-id", this.model.get("food_id"));
       $('.total-price', this.$el).text(this.model.totalPrice());
+      return this;
     }
   });
 
+  var OrderItemList = Backbone.Collection.extend({
+    model: OrderItem,
+    localStorage: new Backbone.LocalStorage("order-items"),
+
+    totalPrice: function() {
+      var totalPrice = 0;
+      this.each(function(order) {
+        totalPrice += order.totalPrice();
+      });
+      return totalPrice;
+    },
+
+    increaseOrder: function(order) {
+      var activeOrderItem = this.activeItem(order.get("food_id"));
+      if (activeOrderItem) {
+        activeOrderItem.increaseAmmount();
+      } else {
+        this.create(order);
+      }
+    },
+
+    descreaseOrder: function(order) {
+      var activeOrderItem = this.activeItem();
+      if (activeOrderItem && activeOrderItem.descreaseAmmount() <= 0) {
+        activeOrderItem.destroy();
+      }
+    },
+
+    checkout: function() {
+      /*$.postJSON("/orders", this.toJSON(), function(response) {
+        alert(response);
+      }, 'json');*/
+    },
+
+    resetActiveItem: function() {
+      this.each(function(orderItem) {
+        orderItem.save({active: false});
+      });
+    },
+
+    activeItem: function(foodId) {
+      if (!foodId) {
+        return this.findWhere({"active": true});
+      } else {
+        this.resetActiveItem();
+
+        var orderItem = this.findWhere({"food_id": foodId});
+        if (orderItem) {
+          orderItem.active();
+          return orderItem;
+        }
+      }
+    },
+
+    destroy: function() {
+      while (orderItem = this.first()) {
+        orderItem.destroy();
+      }
+    }
+  });
+
+  var orderItemList = new OrderItemList;
+
+
   var AppView = Backbone.View.extend({
+    el: "#order-page", 
 
     events: {
       'click #checkout': 'checkout',
       "click tr.order": "activeOrderItem",
       // TODO magic
-      "click button.foot": "orderFoot",
-      "click #plus-item": "plusItem",
-      "click #minus-item": "minusItem", 
+      //"click button.food": "orderFood",
+      "click #plus-item": "increaseOrder",
+      "click #minus-item": "descreaseOrder", 
+      "click #delete-order": "deleteOrder", 
     },
 
     initialize: function() {
@@ -122,35 +135,49 @@ $(function() {
 
       this.orderTotalPriceText = $('#orderTotalPrice');
 
-      $('.foot').click(function() {
-        $('.foot').removeClass("btn-info");
+      $('.food').click(function() {
+        $('.food').removeClass("btn-info");
         $this = $(this);
         $this.addClass("btn-info");
 
-        var orderItem = { foot_id: $this.attr("data-id"), title: $this.text(), price: $this.attr("data-price"), ammount: 1 };
+        var orderItem = new OrderItem({food_id: $this.attr("data-id"), title: $this.text(), price: $this.attr("data-price"), ammount: 1});
         orderItemList.increaseOrder(orderItem);
       });
+
+      orderItemList.fetch();
     },
 
     render: function() {
       this.orderTotalPrice();
     },
 
-    orderFoot: function() {
-        $('.foot').removeClass("btn-info");
+    orderFood: function() {
+        $('.food').removeClass("btn-info");
         $this = $(e.target);
         $this.addClass("btn-info");
 
-        var orderItem = { foot_id: $this.attr("data-id"), title: $this.text(), price: $this.attr("data-price"), ammount: 1 };
+        var orderItem = { food_id: $this.attr("data-id"), title: $this.text(), price: $this.attr("data-price"), ammount: 1 };
         orderItemList.increaseOrder(orderItem);
     },
 
-    plusItem: function() {
-      orderItemList.activeItem().increaseOrder();
+    activeOrderItem: function(e) {
+      var foodId = $(e.target).parent("tr.order").attr("data-id");
+      orderItemList.activeItem(foodId);
+      return false;
     },
 
-    minusItem: function() {
+    increaseOrder: function(e) {
+      var activeItem = orderItemList.activeItem();
+      if (activeItem) {
+        orderItemList.increaseOrder(activeItem);
+      }
+    },
 
+    descreaseOrder: function() {
+      var activeItem = orderItemList.activeItem();
+      if (activeItem) {
+        orderItemList.descreaseOrder(activeItem);
+      }
     },
 
     addOrder: function(orderItem) {
@@ -165,9 +192,9 @@ $(function() {
     checkout: function() {
       orderItemList.checkout();
     },
-    
-    activeOrderItem: function() {
-      this.$el.addClass("error");
+
+    deleteOrder: function() {
+      orderItemList.destroy();
     }
 
   });
